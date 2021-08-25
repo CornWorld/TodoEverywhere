ad12d405727ce5e3b8849ec7a9e7266089d994a1
CornWorld <cw@had.ink>
2021-08-25 23:18:05 +0800

    [Fix](crypto): Fix the bug of random IV when every time use the library

--- crypto.php
+++ crypto.php
@@ -2,13 +2,21 @@
 	
 	class crypto
 	{
-		static public function encrypt($algo, $str, $key) : string
+		static public function lenIV($algo) : int
 		{
-			return base64_encode(openssl_encrypt($str, $algo, $key, OPENSSL_RAW_DATA, openssl_random_pseudo_bytes(openssl_cipher_iv_length($algo))));
+			return openssl_cipher_iv_length($algo);
+		}
+		static public function generateIV($algo) : string
+		{
+			return openssl_random_pseudo_bytes(self::lenIV($algo));
+		}
+		static public function encrypt($algo, $str, $key, $iv) : string
+		{
+			return base64_encode(openssl_encrypt($str, $algo, $key, OPENSSL_RAW_DATA, $iv));
 		}
 		
-		static public function decrypt($algo, $str, $key) : string
+		static public function decrypt($algo, $str, $key, $iv) : string
 		{
-			return openssl_decrypt(base64_decode($str), $algo, $key, OPENSSL_RAW_DATA, openssl_random_pseudo_bytes(openssl_cipher_iv_length($algo)));
+			return openssl_decrypt(base64_decode($str), $algo, $key, OPENSSL_RAW_DATA, $iv);
 		}
 	}
